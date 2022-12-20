62da1b28961e7fb34539f7d0e0e18ae82b3aa21a
SadikKareem <sadiqabdikarim@gmail.com>
2022-12-20 22:51:03 +0000

    Saved progress on CombineStrings 2022-12-20 22:51:03 +0000

--- CombineStrings/src/main/java/CombineStrings.java
+++ CombineStrings/src/main/java/CombineStrings.java
@@ -8,6 +8,8 @@ public class CombineStrings {
      * @return the combination of strings a and b.
      */
     public String combine(String a, String b){
-        return "";
+        String result = a + b;
+
+        return result;
     }
 }
