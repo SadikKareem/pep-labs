f9ce6268802a749a7ab540e47025462acd7dc15d
SadikKareem <sadiqabdikarim@gmail.com>
2022-12-27 07:25:17 +0000

    Saved progress on Instantiation 2022-12-27 07:25:17 +0000

--- Instantiation/src/main/java/InstantiationExample.java
+++ Instantiation/src/main/java/InstantiationExample.java
@@ -18,6 +18,7 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        InstantiationExample myObject = new InstantiationExample();
+        return myObject;
     }
 }
