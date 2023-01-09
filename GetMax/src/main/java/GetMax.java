69ef202f86f32b00aeb8bb0236326bfb38c2edb4
SadikKareem <sadiqabdikarim@gmail.com>
2023-01-09 16:04:11 +0000

    Saved progress on GetMax 2023-01-09 16:04:11 +0000

--- GetMax/src/main/java/GetMax.java
+++ GetMax/src/main/java/GetMax.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class GetMax {
     /**
      * This method should return the max value of arr (the largest int.)
@@ -7,6 +9,28 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        
+        //i created an object of type int "theMaxNumber" and set it's value to the value of the first index in arr and compared to the rest only changing it when i find higher value.
+        
+        int theMaxNumber = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(theMaxNumber < arr[i]){
+                theMaxNumber = arr[i];
+            }
+        }
+
+        
+        
+        //i could also use the built in "sort()" function in arrays that will sort the array in order smallest to biggest and return the value at the end of the array.
+        /*Arrays.sort(arr);
+         theMaxNumber = arr[arr.length -1];
+        
+
+        return theMaxNumber;
+        */
+
+        return theMaxNumber;
+
+
     }
 }
