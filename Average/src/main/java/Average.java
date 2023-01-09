d5f7f84010a7de15db1497983441ea0cf23fb207
SadikKareem <sadiqabdikarim@gmail.com>
2023-01-09 15:40:12 +0000

    Saved progress on Average 2023-01-09 15:40:12 +0000

--- Average/src/main/java/Average.java
+++ Average/src/main/java/Average.java
@@ -18,6 +18,15 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        int sum = 0;
+        double average = 0;
+        for(int i = 0; i<nums.length; i++){
+            sum = sum + nums[i];
+             average = sum;
+
+            //average = sum / nums.length ;
+        }
+        average = average / nums.length ;
+        return average;
     }
 }
