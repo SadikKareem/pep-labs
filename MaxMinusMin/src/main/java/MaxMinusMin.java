189d7ca17e8d3855b54c5feee4d7ad1d959e2a38
SadikKareem <sadiqabdikarim@gmail.com>
2023-01-13 02:49:00 +0000

    Saved progress on MaxMinusMin 2023-01-13 02:49:00 +0000

--- MaxMinusMin/src/main/java/MaxMinusMin.java
+++ MaxMinusMin/src/main/java/MaxMinusMin.java
@@ -1,4 +1,6 @@
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class MaxMinusMin {
@@ -10,6 +12,19 @@ public class MaxMinusMin {
      * @return the max value of nums minus the min value of nums.
      */
     public int difference(List<Integer> nums){
-        return 0;
+        //Here is my thought process.
+        //the easiest way to get the Min and Max is tow sort the list.
+        Collections.sort(nums);
+        //we know that after sorting the nums, our minimum value is at index 0.
+        int myMin = (nums.get(0));
+        //i am using mvn test that is provided to see these outputs and although the tests fail, i get the sense of the process.
+            //System.out.println("Min value is" + myMin);
+        //now getting the max value, we need the value at index size() -1;
+        int myMax = nums.get(nums.size()-1);
+            //System.out.println("Max value is" + myMax);
+
+            //System.out.println("your list is" + nums);
+        
+        return myMax - myMin;
     }
 }
