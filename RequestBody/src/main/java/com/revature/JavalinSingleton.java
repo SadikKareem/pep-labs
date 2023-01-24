446d8f629be92e5f2544d436519a6daa8ec1da4f
SadikKareem <sadiqabdikarim@gmail.com>
2023-01-24 21:03:56 +0000

    Saved progress on RequestBody 2023-01-24 21:03:56 +0000

--- RequestBody/src/main/java/com/revature/JavalinSingleton.java
+++ RequestBody/src/main/java/com/revature/JavalinSingleton.java
@@ -1,5 +1,9 @@
 package com.revature;
 
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.javalin.Javalin;
 
 /**
@@ -18,6 +22,23 @@ public class JavalinSingleton {
          */
         app.post("/problem1", ctx -> {
                 //implement logic here
+                //first step is getting the json from the request body.
+                String jsonString = ctx.body();
+
+                //using jackson "objectMapper" to convert that json in to song object
+                ObjectMapper om = new ObjectMapper();
+                Song song = om.readValue(jsonString, Song.class);
+
+                //using that object to retrieve last name
+                //song.getArtistName();
+                String theArtistName = song.getArtistName();
+
+                //using jackson "writeValueAsString" to turn the retieved value back to json
+                String stringToBeReturned = om.writeValueAsString(theArtistName);
+
+                //returning that json string
+                ctx.result(stringToBeReturned);
+                
         });
 
         /**
@@ -29,6 +50,23 @@ public class JavalinSingleton {
          */
         app.post("/problem2", ctx -> {
                //implement logic here
+
+               //get the json string from the body
+               String jsoString = ctx.body();
+
+               //use objectMapper to convert the json into a son object.
+               ObjectMapper om = new ObjectMapper();
+               Song song = om.readValue(jsoString, Song.class);
+
+               //change artist to "beatles"
+               song.setArtistName("Beatles");
+              
+               //use "WriteValueAsString" to turn string back to json.
+               String theStringToBeReturned = om.writeValueAsString(song);
+
+               //return that json string.
+               ctx.result(theStringToBeReturned);
+               
         });
 
 
