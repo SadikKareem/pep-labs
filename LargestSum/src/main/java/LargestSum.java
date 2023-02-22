c7483d60a2f86748bac3e4cd26bf220f43f5ce67
SadikKareem <sadiqabdikarim@gmail.com>
2023-02-22 03:25:42 +0000

    Saved progress on LargestSum 2023-02-22 03:25:42 +0000

--- LargestSum/src/main/java/LargestSum.java
+++ LargestSum/src/main/java/LargestSum.java
@@ -10,6 +10,26 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        //i need to keep track of the max value and second max value;
+        //first make sure those variable have the lowest numbers in them.
+        int maxValue = Integer.MIN_VALUE;
+        int secondMaxValue = Integer.MIN_VALUE;
+        int maxValue2 = Integer.MIN_VALUE;
+        
+        //use a for loop to traverse through nums and update maxValue and secondMaxValue
+        for(int i = 0; i < nums.size(); i++){
+            //i need a temporary variable to compare to my out of this scope variables and change accordingly
+            int temp = nums.get(i);
+            if(temp >= maxValue){
+                maxValue2 = maxValue;
+                //now am gonna put the number i found inside the maxValue variable
+                maxValue = temp;
+            }else if(temp > maxValue2){
+                maxValue2 = temp;
+            }if(temp > secondMaxValue && temp < maxValue2){
+                secondMaxValue = temp;
+            }
+        }
+        return Math.max(maxValue + secondMaxValue, maxValue + maxValue2);
     }
 }
