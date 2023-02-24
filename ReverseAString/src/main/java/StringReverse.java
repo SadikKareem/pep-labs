0922140b15bcf9b126f9a8ccb0b0be29dbe9cc16
SadikKareem <sadiqabdikarim@gmail.com>
2023-02-24 23:42:06 +0000

    Saved progress on ReverseAString 2023-02-24 23:42:06 +0000

--- ReverseAString/src/main/java/StringReverse.java
+++ ReverseAString/src/main/java/StringReverse.java
@@ -14,7 +14,7 @@ public class StringReverse {
      */
     public String reverse(String str){
         String reversedString = "";
-        for(int i = str.length() -1; i >= 0; i++){
+        for(int i = str.length() -1; i >= 0; i--){
             reversedString = reversedString + str.charAt(i);
         }
         return reversedString;
